7ad4396fb653f960a0bcdc556d24baa06df56b0a
Shreyas Muthkur <shreyasspidy@gmail.com>
2024-02-22 17:55:52 -0500

    Added Column and refined edit text with list to be displayed at center

--- app/src/main/java/com/shreyasmp/blankproject/view/MainActivity.kt
+++ app/src/main/java/com/shreyasmp/blankproject/view/MainActivity.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shreyasmp.blankproject.ui.theme.BlankProjectTheme
 import com.shreyasmp.blankproject.viewmodel.InstrumentViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class MainActivity : ComponentActivity() {
 
@@ -49,16 +59,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun SearchViewBar(viewModel: InstrumentViewModel) {
 
+    var texxtFieldValue by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
     val instList by viewModel.instrumentList.observeAsState()
 
-    LazyColumn(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        instList?.let {
-            items(it) { instrument ->
-                instrument.name?.let { name ->
-                    Text(text = name)
+        TextField(value = texxtFieldValue, onValueChange = { newValue ->
+            texxtFieldValue = newValue
+        },
+            maxLines = 1,
+            placeholder = {
+                Text(text = "Enter type")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        LazyColumn(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            instList?.let {
+                items(it) { instrument ->
+                    instrument.name?.let { name ->
+                        Text(text = name)
+                    }
                 }
             }
         }
